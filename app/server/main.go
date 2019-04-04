54e4427a1c702f41b4d474e6868da55f76b5270c
iCell <icell.vip@gmail.com>
2019-04-04 16:56:34 +0900

    add mixin monitor

--- app/server/main.go
+++ app/server/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/EOSLaoMao/watchdog/internal/engine"
 	"github.com/EOSLaoMao/watchdog/internal/eos"
+	"github.com/EOSLaoMao/watchdog/internal/mixin"
 )
 
 func main() {
 	go eos.CheckUnpaidBlocks()
 	go eos.Listen()
+
+	go mixin.Listen()
 
 	r := engine.E()
 	r.Run()
