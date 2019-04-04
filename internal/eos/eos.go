ecb5683f4c65e7570f0c329ecbed95ebd89bc929
iCell <icell.vip@gmail.com>
2019-04-04 12:21:26 +0900

    add eos ranking

--- internal/eos/eos.go
+++ internal/eos/eos.go
@@ -7,17 +7,19 @@ import (
 )
 
 const (
-	BlockProduceTime = 12 * 0.5
-	ListenBlockPath  = "/eos/block/status"
-	bpCount          = 21
-	bpName           = "eoslaomaocom"
+	BlockProduceTime  = 12 * 0.5
+	ListenBlockPath   = "/eos/block/status"
+	ListenRankingPath = "/eos/bpranking"
+	bpCount           = 21
+	bpName            = "eoslaomaocom"
 )
 
 func Listen() {
 	http.HandleFunc(ListenBlockPath, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("....")
 		switch bs.Status {
 		case BlockStatusPrepare:
-			w.WriteHeader(200)
+			w.WriteHeader(204)
 			w.Write([]byte(fmt.Sprintf("preparing EOS monitor %v", time.Now())))
 		case BlockStatusOK:
 			w.WriteHeader(200)
@@ -32,6 +34,20 @@ func Listen() {
 		case BlockStatusDown:
 			w.WriteHeader(502)
 			w.Write([]byte(bs.Status))
+		}
+	})
+	http.HandleFunc(ListenRankingPath, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("xxxx")
+		switch {
+		case ranking == 0:
+			w.WriteHeader(204)
+			w.Write([]byte(fmt.Sprintf("getting %s ranking %v", bpName, time.Now())))
+		case ranking > 3:
+			w.WriteHeader(502)
+			w.Write([]byte(fmt.Sprintf("the %s ranking is seriously declining to %d", bpName, ranking)))
+		default:
+			w.WriteHeader(200)
+			w.Write([]byte(fmt.Sprintf("current %s ranking is %d", bpName, ranking)))
 		}
 	})
 }
