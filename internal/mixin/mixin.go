c4e2f2baf54004bc8b67a3b797bc44b56e12388b
iCell <icell.vip@gmail.com>
2019-04-04 16:58:58 +0900

    Feature/mixin (#2)
    * rename status
    
    * add eos ranking
    
    * add eos ranking
    
    * use gin to server http request
    
    * format the message
    
    * add mixin monitor
    
    * add mixin monitor

---
+++ internal/mixin/mixin.go
@@ -0,0 +1,66 @@
+package mixin
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/EOSLaoMao/watchdog/internal/engine"
+	"github.com/gin-gonic/gin"
+)
+
+var mixinPaths = map[string]string{
+	"node0": "http://mixin-node0.eoslaomao.com:1443",
+	"node1": "http://mixin-node1.eoslaomao.com:1443",
+}
+
+const (
+	StatusOK      string = "OK"
+	StatusTimeout string = "Timeout"
+	StatusFailure string = "Failure"
+)
+
+func Listen() {
+	for n, u := range mixinPaths {
+		serve(n, u)
+	}
+}
+
+func serve(name string, url string) {
+	engine.E().GET(fmt.Sprintf("/mixin/%s/status", name), func(c *gin.Context) {
+		switch getMixinInfo(url) {
+		case StatusOK:
+			c.String(200, "<b>%s</b> in good condition :), %v", name, time.Now().Format(time.RFC1123))
+		case StatusTimeout:
+			c.String(502, "request timeout for <b>%s</b>, %v", name, time.Now().Format(time.RFC1123))
+		case StatusFailure:
+			c.String(502, "<b>%s maybe unavailable</b>, %v", name, time.Now().Format(time.RFC1123))
+		}
+	})
+}
+
+func getMixinInfo(url string) string {
+	json := []byte(`{"method": "getinfo"}`)
+	body := bytes.NewBuffer(json)
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", url, body)
+
+	req.Header.Add("Content-Type", "application/json; charset=utf-8")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Failure : ", err)
+		if err, ok := err.(net.Error); ok && err.Timeout() {
+			return StatusTimeout
+		}
+		return StatusFailure
+	}
+
+	if resp.StatusCode != 200 {
+		return StatusFailure
+	}
+
+	return StatusOK
+}
