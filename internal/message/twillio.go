030e953471bf187b43ebbe5f8fcc71e0f1661cbf
iCell <icell.vip@gmail.com>
2019-05-20 09:29:51 +0800

    make voice call to multiple numbers

--- internal/message/twillio.go
+++ internal/message/twillio.go
@@ -2,8 +2,10 @@ package message
 
 import (
 	"fmt"
-	"github.com/sfreiberg/gotwilio"
 	"os"
+	"strings"
+
+	"github.com/sfreiberg/gotwilio"
 )
 
 func MakeVoiceCall() {
@@ -13,16 +15,18 @@ func MakeVoiceCall() {
 	twilio := gotwilio.NewTwilioClient(accountSid, authToken)
 
 	from := os.Getenv("TWILLIO_FROM")
-	to := os.Getenv("TWILLIO_TO")
+	tos := os.Getenv("TWILLIO_TO")
 
-	callbackParams := gotwilio.NewCallbackParameters("https://demo.twilio.com/welcome/voice/")
-	_, exp, err := twilio.CallWithUrlCallbacks(from, to, callbackParams)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	if exp != nil {
-		fmt.Println(exp.Message)
-		return
+	for _, to := range strings.Split(tos, ",") {
+		callbackParams := gotwilio.NewCallbackParameters("https://demo.twilio.com/welcome/voice/")
+		_, exp, err := twilio.CallWithUrlCallbacks(from, to, callbackParams)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if exp != nil {
+			fmt.Println(exp.Message)
+			continue
+		}
 	}
 }
